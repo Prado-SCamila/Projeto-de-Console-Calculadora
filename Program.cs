bc17265897274b9a1f449c16b10f3b226461885d
Prado-SCamila <prado.camila.910423@gmail.com>
2020-11-16 13:52:19 -0300

    Projeto finalizado

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Projeto_de_Console_Calculadora
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+                        
+            Console.WriteLine("Digite o primeiro número.");
+            float num1 = float.Parse(Console.ReadLine());
+            Console.WriteLine("Digite o segundo número.");
+            float num2 = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite uma operação.");
+            string operacao = Console.ReadLine();
+
+                switch(operacao){
+                case "soma":
+               float resultado = num1+num2;
+                break;
+
+                case "subtracao":
+               float resultado = num1-num2;
+                break;
+
+                case "divisao":
+               float resultado = num1/num2;
+                break;
+
+                case "multiplicacao":
+               float resultado = num1*num2;
+                break;
+
+                default:
+                Console.WriteLine("Operação Inválida!");
+                break;
+            }
+                Console.WriteLine("O resultado é:"+resultado);
+        }
+
+    }
+}
